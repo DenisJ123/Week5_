f3f9a9943dab9ee7e17bc846fc1798679a0989fe
denis <G00403126@atu.ie>
2024-10-21 21:17:28 +0100

    Exercise 2

---
+++ src/main/java/ie/atu/week5_/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package ie.atu.week5_;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
